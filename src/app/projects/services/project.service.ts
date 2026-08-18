0df328b4de5f5a04ce473fafd19b74a9fdd1ddce
Dawid Nosal <dawidnosal95@gmail.com>
2026-08-18 22:21:14 +0200

    feat: critical priority

--- src/app/projects/services/project.service.ts
+++ src/app/projects/services/project.service.ts
@@ -1,24 +1,23 @@
 import { Injectable, signal } from '@angular/core';
 import { Project } from '../../issues/models/project.model';
 
+export interface UserOption {
+  id: number;
+  displayName: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
-
-export interface UserOption{
-    id: number;
-    displayName: string
-}
-
 export class ProjectService {
   readonly projects = signal<Project[]>([
-    {id: 1, key:"PRJ", name:"Mini-Jira"},
-    {id: 2, key:"LAB", name:"Home Lab"},
+    { id: 1, key: 'PRJ', name: 'Mini-Jira' },
+    { id: 2, key: 'LAB', name: 'Home Lab' },
   ]);
-  
+
   readonly users = signal<UserOption[]>([
-    {id: 1, displayName: "Dawid"},
-    {id: 2, displayName: "Anna"},
-    {id: 3, displayName: "Piotr"},
+    { id: 1, displayName: 'Dawid' },
+    { id: 2, displayName: 'Anna' },
+    { id: 3, displayName: 'Piotr' },
   ]);
 }
